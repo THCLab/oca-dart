8e51a7d826e54e75142a751677e1a092b137e55c
gredajustyna <83274413+gredajustyna@users.noreply.github.com>
2023-04-11 12:25:10 +0200

    Update functions.dart

--- lib/functions.dart
+++ lib/functions.dart
@@ -238,7 +238,7 @@ Widget getWidgetFromJSON (WidgetData data, BuildContext context){
 
 Widget getSubmittedWidgetFromJSON (Map<String, dynamic> widgetMap, BuildContext context) {
   //var widgetMap = renderFilledForm(map, values);
-  var widget = JsonWidgetData.fromDynamic(widgetMap["map"].jsonData["elements"][0], registry: widgetMap["registry"]);
+  var widget = JsonWidgetData.fromDynamic(widgetMap["map"]["elements"][0], registry: widgetMap["registry"]);
   return widget!.build(context: context);
 }
 
