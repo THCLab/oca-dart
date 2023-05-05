bf5f53e9db3fb6c10d680c8fee407710616874e1
gredajustyna <83274413+gredajustyna@users.noreply.github.com>
2023-05-05 08:31:09 +0200

    boolean fix

--- lib/functions.dart
+++ lib/functions.dart
@@ -423,7 +423,7 @@ Future<WidgetData> initialSteps() async{
               }
             }
           }else{
-            values[attributeName] = registryValues[key];
+            values[attributeName] = registryValues[key].toString();
           }
         }
       }
